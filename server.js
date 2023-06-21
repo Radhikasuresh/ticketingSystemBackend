6d20bdade0cda8d1a0f2399179f2cfacd90f2a0e
Radhikasuresh <radhikasuresh550@gmail.com>
2023-06-21 16:22:00 +0530

    user -> register & login function created

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
+
+// routes
+import userRoute from "./routes/userRoute.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.json());
+app.use("/api/users", userRoute);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => console.log("listening on port", PORT));
