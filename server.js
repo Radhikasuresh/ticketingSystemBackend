297b744e65991be47c785334815559b65c9b56e7
Radhikasuresh <radhikasuresh550@gmail.com>
2023-06-25 16:09:49 +0530

    cors middleware added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
-
+import cors from "cors";
 // routes
 import userRoute from "./routes/userRoute.js";
 import ticketRoute from "./routes/ticketRoute.js";
@@ -11,6 +11,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/tickets", ticketRoute);
