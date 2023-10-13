297d984b1cd99b2ff124fee30ff67a609ad09cb7
Radhikasuresh <radhikasuresh550@gmail.com>
2023-10-13 17:39:28 +0530

    cors methods

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://peaceful-cranachan-00a32d.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 const server = http.createServer(app);
 
 const connectedSockets = new Set();
